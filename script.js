d60a9b6fd17a478e228198c8f8ae2318ab122e42
beatricemagana <beemag06@gmail.com>
2020-07-29 16:48:30 -0700

    refactor event listener

--- script.js
+++ script.js
@@ -21,31 +21,19 @@ function isValidEmail(email) {
     return re.test(String(email).toLowerCase());
 }
 
+function checkRequired(inputArray) {
+    inputArray.forEach(function(input){
+        if(input.value.trim() === '') {
+            showError(input, `${input.id} is required`)
+        } else {
+            showSucess(input);
+        }
+    });
+}
+
 form.addEventListener('submit', function(e){
     e.preventDefault(); 
 
-    if(username.value === '') {
-        showError(username, 'Username is required');
-    } else {
-        showSucess(username);
-    }
-    if(email.value === '') {
-        showError(email, 'Email is required');
-    } else if (!isValidEmail(email.value)){
-        showError(email, 'Email is not valid')
-
-    }else {
-        showSucess(email);
-    }
-    if(password.value === '') {
-        showError(password, 'Password is required');
-    } else {
-        showSucess(password);
-    }
-    if(password2.value === '') {
-        showError(password2, 'Confirm password is required');
-    } else {
-        showSucess(password2);
-    }
+    checkRequired([username, email, password, password2]);
 });
 
