34106ad1fbf2ad60884132d50acade57559d587f
beatricemagana <beemag06@gmail.com>
2020-07-29 17:03:09 -0700

    add field validation

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
+const password2 = document.getElementById('confirmation');
 
 function showError(input,message) {
     const formControl = input.parentElement;
@@ -16,10 +16,14 @@ function showSucess(input) {
     formControl.className = 'form-control sucess';
 };
 
-function isValidEmail(email) {
+function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
+    if(re.test(input.value.trim())) {
+        showSucess(input);
+    } else {
+        showError(input, `Email is not valid`);
+        }
+};
 
 function checkRequired(inputArray) {
     inputArray.forEach(function(input){
@@ -29,11 +33,31 @@ function checkRequired(inputArray) {
             showSucess(input);
         }
     });
-}
+};
+
+function checkLength(input, min, max) {
+    if(input.value.length < min) {
+        showError(input, `${input.id} must be at least ${min} characters`);
+    } else if(input.value.length > max) {
+        showError(input, `${input.id} must be less than ${max} characters`);
+    } else {
+        showSucess(input);
+    }
+};
+
+function checkPasswordsMatch(input1, input2) {
+    if(input1.value !== input2.value) {
+        showError(input2, 'Passwords do not match');
+    }
+};
 
 form.addEventListener('submit', function(e){
     e.preventDefault(); 
 
     checkRequired([username, email, password, password2]);
+    checkLength(username, 6, 15);
+    checkLength(password, 8, 25);
+    checkEmail(email);
+    checkPasswordsMatch (password, password2);
 });
 
